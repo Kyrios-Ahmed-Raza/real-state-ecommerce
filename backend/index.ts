43c597ae7709f828df9b9ff01a1695830ae35d23
Kyrios-Ahmed-Raza <envixiously@gmail.com>
2024-04-18 11:29:11 +0500

    "Added json

--- backend/index.ts
+++ backend/index.ts
@@ -2,8 +2,26 @@ import express from "express";
 const app = express();
 import { PORT } from "./config";
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("You're at home page!");
+  res.json([
+    {
+      name: "Ahmed",
+      age: 16,
+      gender: "Male",
+    },
+    {
+      name: "Areeba",
+      age: 21,
+      gender: "Female",
+    },
+    {
+      name: "Abdullah",
+      age: 32,
+      gender: "Male",
+    },
+  ]);
 });
 
 app.listen(PORT, () =>
